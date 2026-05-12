5e4289de0dcb7865e3684b92e4baed7e015af4c0
EmberGamez <eacinta@chicagotechacademy.org>
2026-05-12 11:38:04 -0500

    vid 5

--- Source/RapidFPS/PlayerCharacter.cpp
+++ Source/RapidFPS/PlayerCharacter.cpp
@@ -41,10 +41,12 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Jump);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
 		EnhancedInputComponent->BindAction(ShootAction, ETriggerEvent::Started, this, &APlayerCharacter::Shoot);
 		EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Reload);
+
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 	}
 }
 
@@ -68,7 +70,7 @@ void APlayerCharacter::Move(const FInputActionValue& Value)
 void APlayerCharacter::Jump(const FInputActionValue& Value)
 {
 	
-	UE_LOG(LogTemp, Warning, TEXT("You're Jumping, Not."));
+	//UE_LOG(LogTemp, Warning, TEXT("You're Jumping, Not."));
 }
 
 void APlayerCharacter::Look(const FInputActionValue& Value)
